37164bbe6b084f58274ff8137fe136a4ced0c5a5
Guilherme Costa <gui529@hotmail.com>
2016-10-26 11:36:23 -0400

    Added a driver

---
+++ src/evoting/votingDriver.java
@@ -0,0 +1,19 @@
+package evoting;
+
+public class votingDriver {
+
+	public static void main(String[] args) {
+
+		//Test Main Driver
+			Candidate candidate1 = new Candidate();
+			Candidate candidate2 = new Candidate();
+
+			candidate1.setName("Trump");
+			candidate2.setName("Hillary");
+
+			System.out.println(candidate1.getName());
+			System.out.println(candidate2.getName());
+
+	
+	}//end of main
+}//end of driver
