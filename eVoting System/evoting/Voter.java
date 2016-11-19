c4c782b5f5b50b1a4ac8cf0eefda7d7ba7730007
joetschell <joetschell@hotmail.com>
2016-11-18 23:13:24 -0500

    while loop better

--- eVoting System/evoting/Voter.java
+++ eVoting System/evoting/Voter.java
@@ -47,16 +47,7 @@ public class Voter {
 		chosenCandidate = null;
 		
 	
-		if (Choice ==1){
-			System.out.printf("You have chosen candidate %s, Is this correct?%n", candidate1.getName());
-			chosenCandidate = candidate1;
-		}
-		else if (Choice ==2){
-			System.out.printf("You have chosen candidate %s , Is this correct?%n", candidate2.getName());
-			chosenCandidate = candidate2;
-		}
-		else{
-			while(Choice != 1 && Choice != 2){
+		while(Choice != 1 && Choice != 2){
 			System.out.println("Sorry, that is not a valid choice.  Please enter 1 or 2.");
 			Choice = sc.nextInt();
 			}
@@ -68,8 +59,6 @@ public class Voter {
 				System.out.printf("You have chosen candidate %s , Is this correct?%n", candidate2.getName());
 				chosenCandidate = candidate2;
 			}
-			
-		}
 		
 		System.out.println("1. YES");
 		System.out.println("2. NO");
